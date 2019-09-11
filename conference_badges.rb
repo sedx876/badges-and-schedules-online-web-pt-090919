aad2ff0b498f9d6579842b013986e457ec8232e8
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 12:40:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,16 @@
 # Write your code here.
-
-
-def badge_maker
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name) do
+  
+end
+
+
+
+def batch_badge_maker
+#attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 attendees.each do |name|
   puts "Hello, my name is #{name}"
 end
 end
 
-badge_maker
