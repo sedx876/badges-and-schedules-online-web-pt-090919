446b7e0d526aad09f72bda6563cd2b319c53befe
Sharon Watkins <swatkins876@gmail.com>
2019-09-11 12:47:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name) do
+def badge_maker(name) 
   puts "Hello, my name is #{name}"
 end
 
